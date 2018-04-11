fd2a538721f5addd3345f0183a2f61b6c1ce7e0b
donatPeter <peterdonat.bme@gmail.com>
2018-04-11 18:42:38 +0200

    task#3

--- src/ValidationComponent/ValidationComponent.js
+++ src/ValidationComponent/ValidationComponent.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
 const validationComponent = (props) => {
+
+  const minLength = 5;
+  const outputString = props.length >= minLength ? 'Text long enough' : 'Text too short';
+
   return (
     <div>
-      <p>{props.length}</p>
+      <p>{outputString}</p>
     </div>
   )
 }
