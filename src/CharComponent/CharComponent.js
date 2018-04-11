ac903b31e279ad595cd98685c30f4ac8101f88d7
donatPeter <peterdonat.bme@gmail.com>
2018-04-11 23:18:59 +0200

    task#4

---
+++ src/CharComponent/CharComponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './CharComponent.css';
+
+const charComponent = () => {
+  return (
+    <div className="charComponent" />
+  )
+}
+
+export default charComponent;
